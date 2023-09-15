15d6088958c147bc35275f43827e34d3b390c937
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-15 04:03:16 -0400

    Importar dropzone en sass y js de resources para compilarlos con webpack

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+// const { Dropzone } = require("dropzone/src/dropzone");
+// const { Dropzone } = require("dropzone/dist/dropzone");
+
+// Requerir dropzone en el proyecto
+const { Dropzone } = require("dropzone");
+
+window.Dropzone = Dropzone;
